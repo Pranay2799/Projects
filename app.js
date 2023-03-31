677be26d3a9903f0df11d5d6a9bb104986d42faa
Pranay2799 <pranaychintu27@gmail.com>
2023-03-31 14:53:41 +0530

    adding

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
-
-
 const express=require('express');
+const bodyparser=require('body-parser');
 const app=express();
 
-app.use((req,res,next)=>{
-    console.log('Pranay is Hear');
-    next();
+app.use(bodyparser.urlencoded({extended:false}));
+
+app.use('/add-product',(req,res,next)=>{
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="number" name="size"><button type="submit">add</button></form>');
 
 });
-app.use((req,res,next)=>{
-    console.log('Shreya is Hear');
+app.use('/product',(req,res,next)=>{
+    console.log(req.body);
+    res.redirect('/');
+})    
+
+app.use('/',(req,res,next)=>{
     res.send('<html><h2>Pranay says Hi</h2></html>');
 
 });
